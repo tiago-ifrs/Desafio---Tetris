f3057a3103ca88762d8fb59619fef082e6668adb
Tiago Dermann <tiago.dermann@sequor.com.br>
2021-07-05 13:22:18 -0300

    correções na classe jogo e desnho de tabuleiro

--- Jogo.cs
+++ Jogo.cs
@@ -4,104 +4,120 @@ using System.Windows.Forms;
 
 public class Jogo
 {
-    private readonly Peca at;
-    private readonly Peca prox;
+    private Peca at;
+    private Peca prox;
+
+    private readonly Tabuleiro tabuleiro;
+    private readonly Panel janelaAtual;
+    private readonly Panel janelaProx;
+
     private readonly int ytab;   // coordenada y do tabuleiro
-    private readonly bool pecaNova = false;
+    private void GeraProx() 
+    {
+        if (this.prox != null)
+        {
+            this.prox.ap = janelaAtual;
+            this.at = this.prox;
+            at.AtualizaPeca();
+        }
+        this.prox = new Peca(tabuleiro, janelaProx);
+    }
+    
     public Jogo(Tabuleiro tabuleiro, Panel janelaAtual, Panel janelaProx)
     {
         this.at = new Peca(tabuleiro, janelaAtual);
         this.prox = null;
-
-        this.pecaNova = false;
+        this.tabuleiro = tabuleiro;
+        this.janelaAtual = janelaAtual;
+        this.janelaProx = janelaProx;
 
         //condições iniciais:
         bool over = false;
-        //bool colisaoX = false;
-        bool colisaoY = false;
-        bool tabXLivre = true;
-
+        bool pecaNova = true;
+        
+        int ypec = -1;
+        int ul = -1;
         while (!over)
         {
-            if(pecaNova)
+            if (pecaNova)
             {
-                if(this.prox != null) 
-                {
-                    this.prox.ap = janelaAtual;
-                    this.at = this.prox;
-                    at.AtualizaPeca();
-                }
-
-                this.prox = new Peca(tabuleiro, janelaProx);
+                GeraProx();               
                 pecaNova = false;
-
             }
-            
-            for (ytab = 0; ytab < tabuleiro.nlin; ytab++) // percorre as linhas do tabuleiro
-            {
-                int ypec;
-                int mask = at.QLinhas-1;
+            //atualiza o índice de peça após a geração de cada peça nova ou em condição inicial:
+            ul = at.QLinhas - 1;
+            ypec = ul;
 
-                //ypec = (~ytab & at.QLinhas)-1;         //inicializa posição y das peças com ytab de offset          
-                ypec = (mask&~ytab);         //inicializa posição y das peças com ytab de offset          
+            //condições atualizadas a cada nova peça
+            //bool colisaoX = false;
+            bool colisaoY = false;
+            bool tabXLivre = true;
+            bool terminaPeca = false;
+
+            for (ytab = 0; ytab < tabuleiro.nlin-1; ytab++) // percorre as linhas do tabuleiro
+            {
                 if (tabXLivre == true) // os 4 espaços inferiores estão vazios, peça cai
                 {
-                    if (ytab < at.QLinhas)
+                    if (ypec >= 0)
                     {
                         tabuleiro.PoePeca(at, ytab, ypec);
+                        ypec--;
                     }
-                    else
+                    else //ypec == -1
                     {
-                        tabuleiro.TerminaPeca(at);
-                        pecaNova = true;
+                        if (!pecaNova)
+                        {
+                            //tabuleiro.PoePeca(at, ytab, ypec);
+                            //tabuleiro.TerminaPeca(at);
+                            pecaNova = true;
+                        }
                     }
                     if (!colisaoY)
                     {
-                        tabuleiro.MoveY(at, ytab);
-                        Thread.Sleep(1000);
+                        for (int xtab = 0; xtab < at.QColunas(ul); xtab++)        // percorre as colunas do tabuleiro abaixo da peça
+                        {
+                            if (!colisaoY)
+                            {
+                                if (tabuleiro.Matrix[ytab + 1][xtab].Valor == 0) //local vazio no tabuleiro
+                                {
+                                    colisaoY = false; //não há colisão
+                                    tabXLivre = true; // posição livre
+                                }
+                                else // local ocupado no tabuleiro
+                                {
+                                    tabXLivre = false;  // posição ocupada
+                                                        // pode haver colisão se a peça for 1
+                                    if (at.Ponto(ul, xtab) == 1) // peça está ocupando posição x
+                                    {
+                                        colisaoY = true;
+                                        //empilhar peças
+                                    }
+                                } //else local ocupado no tabuleiro			
+                            }
+                        } //for xtab
+                        if (!colisaoY)
+                        {
+                            tabuleiro.MoveY(at, ytab);
+
+                            tabuleiro.LimpaAcima(at, ytab, ypec);
+                            Thread.Sleep(1000);
+                        }
+                        
                     }
                 } // fim if tabXlivre
-                for (int xtab = 0; xtab < at.QColunas(at.QLinhas-1); xtab++)        // percorre as colunas do tabuleiro abaixo da peça
+                else
                 {
-                    if (tabuleiro.Matrix[ytab][xtab].Valor == 0) //local vazio no tabuleiro
+                    if (colisaoY)
                     {
-                        colisaoY = false; //não há colisão
-                        tabXLivre = true; // posição livre
-                    }
-                    else // local ocupado no tabuleiro
-                    {
-                        tabXLivre = false;  // posição ocupada
-                                            // pode haver colisão se a peça for 1
-                        if (at.Ponto(ypec, xtab) == 1) // peça está ocupando posição x
+                        if(ytab < this.prox.QLinhas)
                         {
-                            colisaoY = true;
-                            //empilhar peças
+                            MessageBox.Show("Game Over");
+                            over = true;
                         }
-
-                        // comparar as 4 colunas da peça onde ela for cair no tabuleiro
-                        // coordenadas do tabuleiro (xtab, ytab)
-
-                        // percorre as colunas da peça se a posição não estiver livre no tabuleiro:
-                        /*
-                        for (xpec = 0; xpec < linTabAt.Length - 1; xpec++)
-                        {
-                            if (ultLinPecAt[xpec] == 1)
-                            {
-                                if (linTabAt[xpec] == 1)
-                                {
-                                    colisaoY = true;
-                                    break;
-                                }
-                            }
-                            else //uma das posiçoes é 0
-                            {
-                                colisaoY = false;
-                            }
-                        } // terminou de percorrer as colunas da peça
-                        */
-                    } //else local ocupado no tabuleiro			
-                } //for xtab
+                    }
+                }
             } //for ytab
+            
         }//while !over
     } //construtor
 }
--- Tabuleiro.cs
+++ Tabuleiro.cs
@@ -25,7 +25,9 @@ public class Tabuleiro
 
     public void PoePeca(Peca p, int ytab, int ypec)
     {
-        for (int xpec = 0; xpec < p.QColunas(ypec); xpec++)
+        int ul = p.QLinhas - 1;
+        int uc = p.QColunas(ul);
+        for (int xpec = 0; xpec < uc; xpec++)
         {
             Matrix[0][xpec].Valor = p.Ponto(ypec, xpec);
 
@@ -56,17 +58,40 @@ public class Tabuleiro
             }
         }
     }
-    public void TerminaPeca(Peca p)
+
+    public void LimpaAcima(Peca p, int ytab, int ypec)
     {
         int ul = p.QLinhas - 1;
         int uc = p.QColunas(ul);
-
-        //atualiza posições acima no tabuleiro
-        for (int xpec = 0; xpec < uc; xpec++)
+        if (ypec != -1)
         {
-            Matrix[0][xpec].Valor = 0;
-            Matrix[0][xpec].BackColor = Color.AntiqueWhite;
-            Matrix[0][xpec].Refresh();
+            
+            //atualiza posições acima no tabuleiro
+            for (int xpec = 0; xpec < uc; xpec++)
+            {
+                if (p.Ponto(ypec, xpec) == 0)
+                {
+                    Matrix[0][xpec].Valor = 0;
+                    Matrix[0][xpec].BackColor = Color.AntiqueWhite;
+                }
+                else 
+                {
+                    Matrix[0][xpec].Valor = p.Ponto(ypec, xpec); //1
+                    Matrix[0][xpec].BackColor = p.Cor;
+                    
+                }
+                Matrix[0][xpec].Refresh();
+            }
+
+        }
+        else //ypec == -1, terminou de gerar a peça
+        {
+            for (int xpec = 0; xpec < uc; xpec++)
+            {
+                Matrix[0][xpec].Valor = 0;
+                Matrix[0][xpec].BackColor = Color.AntiqueWhite;
+                Matrix[0][xpec].Refresh();
+            }
         }
     }
 }
