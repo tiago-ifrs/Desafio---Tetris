d558dc3085725a7b2d5c56783ce58083e6af782d
tiago <tiago@NOVO-PC>
2021-07-05 02:37:52 -0300

    alteração no movimento das peças no tabuleiro

--- Jogo.cs
+++ Jogo.cs
@@ -4,28 +4,26 @@ using System.Windows.Forms;
 
 public class Jogo
 {
-    private Peca at;
-    private Peca prox;
+    private readonly Peca at;
+    private readonly Peca prox;
     private readonly int ytab;   // coordenada y do tabuleiro
-    private readonly Tabuleiro tabuleiro;
-    bool pecaNova = false;
+    private readonly bool pecaNova = false;
     public Jogo(Tabuleiro tabuleiro, Panel janelaAtual, Panel janelaProx)
     {
-        this.tabuleiro = tabuleiro;
         this.at = new Peca(tabuleiro, janelaAtual);
         this.prox = null;
 
         this.pecaNova = false;
 
+        //condições iniciais:
         bool over = false;
-        
         //bool colisaoX = false;
         bool colisaoY = false;
-        bool tabXLivre = false;
+        bool tabXLivre = true;
 
         while (!over)
         {
-            if(!pecaNova)
+            if(pecaNova)
             {
                 if(this.prox != null) 
                 {
@@ -35,13 +33,35 @@ public class Jogo
                 }
 
                 this.prox = new Peca(tabuleiro, janelaProx);
-                pecaNova = true;
+                pecaNova = false;
+
             }
+            
             for (ytab = 0; ytab < tabuleiro.nlin; ytab++) // percorre as linhas do tabuleiro
             {
                 int ypec;
-                ypec = (~ytab & (at.QLinhas)-1);         //inicializa posição y das peças com ytab de offset          
-                for (int xtab = 0; xtab < at.QColunas(ypec); xtab++)        // percorre as colunas do tabuleiro abaixo da peça
+                int mask = at.QLinhas-1;
+
+                //ypec = (~ytab & at.QLinhas)-1;         //inicializa posição y das peças com ytab de offset          
+                ypec = (mask&~ytab);         //inicializa posição y das peças com ytab de offset          
+                if (tabXLivre == true) // os 4 espaços inferiores estão vazios, peça cai
+                {
+                    if (ytab < at.QLinhas)
+                    {
+                        tabuleiro.PoePeca(at, ytab, ypec);
+                    }
+                    else
+                    {
+                        tabuleiro.TerminaPeca(at);
+                        pecaNova = true;
+                    }
+                    if (!colisaoY)
+                    {
+                        tabuleiro.MoveY(at, ytab);
+                        Thread.Sleep(1000);
+                    }
+                } // fim if tabXlivre
+                for (int xtab = 0; xtab < at.QColunas(at.QLinhas-1); xtab++)        // percorre as colunas do tabuleiro abaixo da peça
                 {
                     if (tabuleiro.Matrix[ytab][xtab].Valor == 0) //local vazio no tabuleiro
                     {
@@ -81,69 +101,7 @@ public class Jogo
                         */
                     } //else local ocupado no tabuleiro			
                 } //for xtab
-
-                if (tabXLivre == true) // os 4 espaços inferiores estão vazios, peça cai
-                {
-                    if (pecaNova)
-                    {
-                        PoePeca();
-                    }
-                    if (!colisaoY)
-                    {
-                        MoveTabY();
-                    }
-                } // fim if tabXlivre
             } //for ytab
         }//while !over
     } //construtor
-    private void PoePeca()
-    {
-        int ul = at.QLinhas-1;
-
-        for (int ypec = 0; ypec <= ul; ypec++)
-        {
-            for (int xpec = 0; xpec < at.QColunas(ypec); xpec++)
-            {
-                tabuleiro.Matrix[ypec - ytab][xpec].Valor = at.Ponto(ypec - ytab, xpec);
-
-                if (at.Ponto(ypec - ytab, xpec) == 1)
-                {
-                    tabuleiro.Matrix[ypec - ytab][xpec].BackColor = at.Cor;
-                }
-                else
-                {
-                    tabuleiro.Matrix[ypec - ytab][xpec].BackColor = Color.White;
-                }
-                tabuleiro.Matrix[ypec - ytab][xpec].Refresh();
-            }
-        }
-        pecaNova = false;
-    }
-
-    private void MoveTabY()
-    {
-        int ul = at.QLinhas-1;
-        int uc = at.QColunas(ul);
-
-        for (int ytaux = ytab; ytaux > 0; ytaux--)
-        { 
-            for (int xpec = 0; xpec < uc; xpec++)
-            {
-                //atualiza posição no tabuleiro
-                tabuleiro.Matrix[ytaux][xpec].Valor = tabuleiro.Matrix[ytaux - 1][xpec].Valor;
-                tabuleiro.Matrix[ytaux][xpec].BackColor = tabuleiro.Matrix[ytaux - 1][xpec].BackColor;
-                tabuleiro.Matrix[ytaux][xpec].Refresh();
-            }
-        }
-        if (ytab > 0) 
-        {
-            for (int xpec = 0; xpec < uc; xpec++)
-            {
-                tabuleiro.Matrix[0][xpec].Valor = 0;
-                tabuleiro.Matrix[0][xpec].BackColor = Color.AntiqueWhite;
-                tabuleiro.Matrix[0][xpec].Refresh();
-            }
-        }
-        Thread.Sleep(1000);
-    }
 }
--- Tabuleiro.cs
+++ Tabuleiro.cs
@@ -3,22 +3,70 @@ using System.Windows.Forms;
 
 public class Tabuleiro
 {
-	public readonly int nlin = 16;
-	public readonly int ncol = 10;
-	public RetanguloTabuleiro[][] Matrix { get; }
-	public Tabuleiro(Panel t)
-	{
-		int l, a, menor;
-		a = t.Height / nlin;
-		l = t.Width / ncol;
-		if (l < a)
-		{
-			menor = l;
-		}
-		else {
-			menor = a;
-		}
-				
-		Matrix=RetanguloTabuleiro.Inicializa(t, nlin, ncol, menor, menor);
-	}
+    public readonly int nlin = 16;
+    public readonly int ncol = 10;
+    public RetanguloTabuleiro[][] Matrix { get; }
+    public Tabuleiro(Panel t)
+    {
+        int l, a, menor;
+        a = t.Height / nlin;
+        l = t.Width / ncol;
+        if (l < a)
+        {
+            menor = l;
+        }
+        else
+        {
+            menor = a;
+        }
+
+        Matrix = RetanguloTabuleiro.Inicializa(t, nlin, ncol, menor, menor);
+    }
+
+    public void PoePeca(Peca p, int ytab, int ypec)
+    {
+        for (int xpec = 0; xpec < p.QColunas(ypec); xpec++)
+        {
+            Matrix[0][xpec].Valor = p.Ponto(ypec, xpec);
+
+            if (p.Ponto(ypec, xpec) == 1)
+            {
+                Matrix[0][xpec].BackColor = p.Cor;
+            }
+            else
+            {
+                Matrix[0][xpec].BackColor = Color.White;
+            }
+            Matrix[0][xpec].Refresh();
+        }
+    }
+    public void MoveY(Peca p, int ytab)
+    {
+        int ul = p.QLinhas - 1;
+        int uc = p.QColunas(ul);
+
+        for (int cont = ytab; cont >= 0; cont--)
+        {
+            for (int xpec = 0; xpec < uc; xpec++)
+            {
+                //atualiza posição abaixo no tabuleiro
+                Matrix[cont + 1][xpec].Valor = Matrix[cont][xpec].Valor;
+                Matrix[cont + 1][xpec].BackColor = Matrix[cont][xpec].BackColor;
+                Matrix[cont + 1][xpec].Refresh();
+            }
+        }
+    }
+    public void TerminaPeca(Peca p)
+    {
+        int ul = p.QLinhas - 1;
+        int uc = p.QColunas(ul);
+
+        //atualiza posições acima no tabuleiro
+        for (int xpec = 0; xpec < uc; xpec++)
+        {
+            Matrix[0][xpec].Valor = 0;
+            Matrix[0][xpec].BackColor = Color.AntiqueWhite;
+            Matrix[0][xpec].Refresh();
+        }
+    }
 }
