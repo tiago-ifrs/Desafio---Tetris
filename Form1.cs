1a4f9c73f32810c37f9b2ebea4572154037df34a
Tiago Dermann <tiago.dermann@sequor.com.br>
2021-07-12 10:00:34 -0300

    Separação dos métodos na classe de jogo

--- Form1.cs
+++ Form1.cs
@@ -5,31 +5,12 @@ namespace Desafio___Tetris
 {
     public partial class Form1 : Form
     {
-        private Peca at;
-        private Peca prox;
+        private Jogo Jogo { get; set; }
+        private bool pause { get; set; }
 
-        private Tabuleiro tabuleiro;
-        private Panel janelaAtual;
-        private Panel janelaProx;
-        private Placar Placar;
-
-        private int ytab;   // coordenada y do tabuleiro
-        private int xtab;   // coordenada x do tabuleiro
-        private bool pause = false;
-        private void GeraProx()
-        {
-            if (this.prox != null)
-            {
-                this.prox.ap = janelaAtual;
-                this.at = this.prox;
-                at.AtualizaPeca();
-            }
-            this.prox = new Peca(tabuleiro, janelaProx);
-        }
         public Form1()
         {
             InitializeComponent();
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -49,6 +30,7 @@ namespace Desafio___Tetris
 
         private void buttonNJ_Click(object sender, EventArgs e)
         {
+            this.pause = false;
             Tetris(panelTabuleiro, panelAtual, panelProx, labelPlacar);
         }
 
@@ -67,67 +49,34 @@ namespace Desafio___Tetris
                 pause = false;
             }
             labelPause.Refresh();
-            while (pause)
+            while (pause) 
             {
-                Wait(1000);
+                Jogo.Espera();
             }
         }
         protected override bool ProcessDialogKey(Keys keyData)
         {
-            Colisao baixo;
             switch (keyData)
             {
                 case (Keys.Up):
                     labelKey.Text = Char.ToString((char)0xe1);
-                    tabuleiro.LimpaPeca(at, ytab, xtab); // precisa limpar o espaço da peça antes de rotacionar
-                    if (at.Rot < 4)
-                    {
-                        at.Rot++;
-                    }
-                    else
-                    {
-                        at.Rot = 0;
-                    }
-                    tabuleiro.DesenhaY(at, ytab, xtab);
+                    Jogo.RotacionaPeca();
+                    
                     return true;
                 case (Keys.Down):
                     labelKey.Text = Char.ToString((char)0xe2);
-                    if (ytab < tabuleiro.nlin)
-                    {
-                        baixo = new Colisao(tabuleiro, at, ytab, xtab);
-
-                        if (baixo.Ycoli == -1)//não houve colisão
-                        {
-                            ytab++;
-
-                        }//if !colisaoY
-                    }
-                    /*
-                    if (ytab < tabuleiro.nlin - at.QLinhas)
-                    {
-                        tabuleiro.LimpaPeca(at, ytab, xtab);//limpa o espaço da peça antes de alterar a variável
-                        ytab++;
-                        tabuleiro.DesenhaY(at, ytab, xtab); //verificar valor de ytab antes de mandar o parâmetro
-                    }
-                    */
+                    Jogo.MoveAbaixo();
+                    
                     return true;
                 case (Keys.Left):
                     labelKey.Text = Char.ToString((char)0xdf);
-                    if (xtab > 0)
-                    {
-                        tabuleiro.LimpaPeca(at, ytab, xtab);//limpa o espaço da peça antes de alterar a variável
-                        xtab--;
-                        tabuleiro.DesenhaY(at, ytab, xtab); //verificar valor de ytab antes de mandar o parâmetro
-                    }
+                    Jogo.MoveEsquerda();
+
                     return true;
                 case (Keys.Right):
                     labelKey.Text = Char.ToString((char)0xe0);
-                    if (xtab < tabuleiro.ncol - at.QColunas(at.QLinhas - 1))
-                    {
-                        tabuleiro.LimpaPeca(at, ytab, xtab);//limpa o espaço da peça antes de alterar a variável
-                        xtab++;
-                        tabuleiro.DesenhaY(at, ytab, xtab); //verificar valor de ytab antes de mandar o parâmetro
-                    }
+                    Jogo.MoveDireita();
+                                        
                     return true;
             };
             // return the key to the base class if not used.
@@ -136,58 +85,18 @@ namespace Desafio___Tetris
         }
 
         public void Tetris(Panel janelaTabuleiro, Panel janelaAtual, Panel janelaProx, Label lbplacar)
-        {
-            //this.tabuleiro = new Tabuleiro(janelaTabuleiro);
-            this.tabuleiro = Tabuleiro.GetInstance(janelaTabuleiro);
-            this.janelaAtual = janelaAtual;
-            this.janelaProx = janelaProx;
+        {           
+            Tabuleiro tabuleiro = Tabuleiro.GetInstance(janelaTabuleiro);
+            
+            this.Jogo = new Jogo(tabuleiro, janelaAtual, janelaProx, lbplacar);
 
-            this.Placar = new Placar(lbplacar, tabuleiro);
-
-            this.at = new Peca(tabuleiro, janelaAtual);
-            this.prox = null;
-
-            //condições iniciais:
             bool over = false;
-            Colisao colisaoY;
             while (!over)
             {
-                GeraProx();
-                xtab = (tabuleiro.ncol - at.QColunas(at.QLinhas - 1)) / 2; // coordenada x inicial da queda de peças
-                colisaoY = null;
-                //for (ytab = 0; ytab < tabuleiro.nlin && colisaoY==false; ytab++) // percorre as linhas do tabuleiro. precisa testar a colisão a cada entrada no loop
-                for (ytab = 0; ytab < tabuleiro.nlin; ytab++) // percorre as linhas do tabuleiro. precisa testar a colisão a cada entrada no loop
-                {
-                    colisaoY = new Colisao(tabuleiro, at, ytab, xtab);
-
-                    if (colisaoY.Ycoli == -1)//não houve colisão
-                    {
-                        //detecção de colisão está limpando a peça, limpar de novo
-                        tabuleiro.LimpaPeca(at, ytab-1, xtab);
-                        tabuleiro.DesenhaY(at, ytab, xtab);
-
-                    }//if !colisaoY
-                    else
-                    {
-                        tabuleiro.DesenhaY(at, ytab - 1, xtab); //desenha na linha anterior se houver colisão
-                        if (ytab == 0) // colisão na 1ª linha, não é detectada pela classe colisão ainda
-                        {
-                            over = true;
-                        }
-                        break;
-                    }
-                    Placar.Atualiza();
-                    Wait(1000);
-                }//for ytab
-            }//while !over
+                over = Jogo.Percorre();   
+            }
             MessageBox.Show("Game Over");
-        } //construtor
-        private void Wait(int ms)
-        {
-            DateTime start = DateTime.Now;
-            while ((DateTime.Now - start).TotalMilliseconds < ms)
-                Application.DoEvents();
-        }
+        } 
     }
 }
 
---
+++ Jogo.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Windows.Forms;
+
+public class Jogo
+{
+    private int Ytab { get; set; }   // coordenada y do tabuleiro
+    private int Xtab { get; set; }   // coordenada x do tabuleiro
+    private Panel JanelaAtual { get; set; }
+    private Panel JanelaProx { get; set; }
+    private Peca At { get; set; }
+    private Peca Prox { get; set; }
+    private Placar Placar { get; set; }
+    private Tabuleiro Tabuleiro { get; set; }
+
+
+
+    public Jogo(Tabuleiro t, Panel jA, Panel jP, Label lP)
+    {
+        this.Tabuleiro = t;
+        this.JanelaAtual = jA;
+        this.JanelaProx = jP;
+        this.Placar = new Placar(lP, Tabuleiro);
+
+    }
+    private void GeraProx()
+    {
+        if (this.Prox != null)
+        {
+            this.Prox.ap = JanelaAtual;
+            this.At = this.Prox;
+            At.AtualizaPeca();
+        }
+        this.Prox = new Peca(Tabuleiro, JanelaProx);
+    }
+    public void Espera()
+    {
+        Wait(1000);
+    }
+    public void RotacionaPeca()
+    {
+        Tabuleiro.LimpaPeca(At, Ytab, Xtab); // precisa limpar o espaço da peça antes de rotacionar
+        if (At.Rot < 4)
+        {
+            At.Rot++;
+        }
+        else
+        {
+            At.Rot = 0;
+        }
+        Tabuleiro.DesenhaY(At, Ytab, Xtab);
+    }
+    public void MoveAbaixo()
+    {
+        Colisao baixo;
+        if (Ytab < Tabuleiro.nlin-1)
+        {
+            baixo = new Colisao(Tabuleiro, At, Ytab+1, Xtab); //detectar colisão uma linha abaixo
+
+            if (baixo.Ycoli == -1)//não houve colisão
+            {
+                Tabuleiro.LimpaPeca(At, Ytab, Xtab);
+                Ytab++;
+                
+                Tabuleiro.DesenhaY(At, Ytab, Xtab);
+            }//if !colisaoY
+        }
+        /*
+        if (colisaoY.Ycoli == -1)//não houve colisão
+        {
+            //detecção de colisão está limpando a peça, limpar de novo
+            Tabuleiro.LimpaPeca(At, Ytab - 1, Xtab);
+            Tabuleiro.DesenhaY(At, Ytab, Xtab);
+
+        }//if !colisaoY
+        else
+        {
+            Tabuleiro.DesenhaY(At, Ytab - 1, Xtab); //desenha na linha anterior se houver colisão
+            if (colisaoY.Ycoli == 0) // colisão na 1ª linha, não é detectada pela classe colisão ainda
+            {
+                return true;
+            }
+            break;
+        }
+        */
+        /*
+        if (ytab < tabuleiro.nlin - at.QLinhas)
+        {
+            tabuleiro.LimpaPeca(at, ytab, xtab);//limpa o espaço da peça antes de alterar a variável
+            ytab++;
+            tabuleiro.DesenhaY(at, ytab, xtab); //verificar valor de ytab antes de mandar o parâmetro
+        }
+        */
+    }
+    public void MoveEsquerda()
+    {
+        if (Xtab > 0)
+        {
+            Tabuleiro.LimpaPeca(At, Ytab, Xtab);//limpa o espaço da peça antes de alterar a variável
+            Xtab--;
+            Tabuleiro.DesenhaY(At, Ytab, Xtab); //verificar valor de ytab antes de mandar o parâmetro
+        }
+    }
+    public void MoveDireita()
+    {
+        if (Xtab < Tabuleiro.ncol - At.QColunas(At.QLinhas - 1))
+        {
+            Tabuleiro.LimpaPeca(At, Ytab, Xtab);//limpa o espaço da peça antes de alterar a variável
+            Xtab++;
+            Tabuleiro.DesenhaY(At, Ytab, Xtab); //verificar valor de ytab antes de mandar o parâmetro
+        }
+    }
+    public bool Percorre()
+    {
+        //condições iniciais:
+        this.At = new Peca(Tabuleiro, JanelaAtual);
+        this.Prox = null;
+        Colisao colisaoY;
+
+        GeraProx();
+        Xtab = (Tabuleiro.ncol - At.QColunas(At.QLinhas - 1)) / 2; // coordenada x inicial da queda de peças
+        colisaoY = null;
+        //for (ytab = 0; ytab < tabuleiro.nlin && colisaoY==false; ytab++) // percorre as linhas do tabuleiro. precisa testar a colisão a cada entrada no loop
+        for (Ytab = 0; Ytab < Tabuleiro.nlin; Ytab++) // percorre as linhas do tabuleiro. precisa testar a colisão a cada entrada no loop
+        {
+            colisaoY = new Colisao(Tabuleiro, At, Ytab, Xtab);
+
+            if (colisaoY.Ycoli == -1)//não houve colisão
+            {
+                //detecção de colisão está limpando a peça, limpar de novo
+                Tabuleiro.LimpaPeca(At, Ytab - 1, Xtab);
+                Tabuleiro.DesenhaY(At, Ytab, Xtab);
+
+            }//if !colisaoY
+            else
+            {
+                Tabuleiro.DesenhaY(At, Ytab - 1, Xtab); //desenha na linha anterior se houver colisão
+                if (colisaoY.Ycoli == 0) // colisão na 1ª linha, não é detectada pela classe colisão ainda
+                {
+                    return true;
+                }
+                break;
+            }
+            Placar.Atualiza();
+            Wait(1000);
+        }
+        return false;
+    }
+    private void Wait(int ms)
+    {
+        DateTime start = DateTime.Now;
+        while ((DateTime.Now - start).TotalMilliseconds < ms)
+            Application.DoEvents();
+    }
+}
