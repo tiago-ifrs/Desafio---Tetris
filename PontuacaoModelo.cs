ac4cfa7c080684f5b5c1dda7c19ea1bfb4846c50
tiago <tiago@NOVO-PC>
2021-07-15 00:52:43 -0300

    Modelagem Pontuação

---
+++ PontuacaoModelo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+public class PontuacaoModelo
+{
+	private int Id { get; set; }
+	private string Nome { get; set; }
+	private int Pontos { get; set; }
+	private int QtdPecas { get; set; }
+	private int Nivel { get; set; }
+	private DateTime TempoJogo { get; set; }
+	private DateTime DataScore { get; set; }
+
+	public PontuacaoModelo()
+	{
+	}
+}
